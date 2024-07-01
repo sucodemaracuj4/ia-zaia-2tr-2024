38739631afaf4a5bddbf59b6deb927fb59ef6660
su <111879114+sucodemaracuj4@users.noreply.github.com>
2024-07-01 11:04:29 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativa = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const caixaResultado1 = document.querySelector(".caixa-principal");
+const perguntas = [
+    {
+        enunciado:"De que maneira podemos abordar as discriminações de classe propagadas nas redes de comunicação e ainda, podendo abranger resoluções frugais no intuito de enaltecer essas comunidades?",
+        alternativas:[
+        "permitindo conexões com o mundo digital",
+        "utilizar redes de comunicação mais acessíveis para divulgar novidades ecológicas e simples que caiba no cotidiano de cada cidadão."    
+        ]
+        
+    },
+]
